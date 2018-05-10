5fd0baa51b3f9f3f47124475b5452cc0b68a08ba
Gabriel de Souza <souzagaabriel@localhost.localdomain>
2018-05-10 18:45:13 -0300

    Testes validados.

--- roles/acesso_remoto/files/x11vnc.sh
+++ roles/acesso_remoto/files/x11vnc.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-/usr/bin/x11vnc -auth guess -noxdamage -rfbport 5900 -shared -forever -bg -xkb -nopw -accept popup:0 &> /dev/null
+/usr/bin/x11vnc -auth guess -noxdamage -rfbport 5900 -shared -forever -bg -xkb -nopw -accept popup:0 > /dev/null 2> /dev/null
