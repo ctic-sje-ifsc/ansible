ff727aafc2bb53aefc5fa952d81babf0872dce77
Gabriel de Souza <souzagaabriel@localhost.localdomain>
2018-05-10 12:25:32 -0300

    Update

--- roles/foxit/files/foxitsoftware/FoxitReader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
- appname="FoxitReader"
-
- selfpath="/opt/foxitsoftware"
- LD_LIBRARY_PATH=$selfpath/lib:$selfpath/platforms:$selfpath/printsupport:$selfpath/rmssdk:$selfpath/sensors:$selfpath/imageformats:$selfpath/platforminputcontexts:$LD_LIBRARY_PATH
- export LD_LIBRARY_PATH
- exec "$selfpath/$appname" "$@"
