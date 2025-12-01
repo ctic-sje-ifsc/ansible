05846e3507ffc13c86047358ef23374c8e6aef2d
Ricardo Martins <ricardo.ifsc@gmail.com>
2025-12-01 12:46:02 -0300

    removido config anterior

--- roles/papel_de_parede_e_icones/files/trocaplanodefundo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dconf write /org/mate/desktop/background/picture-filename "'/home/fundo_ifsc_sj.jpg'"
-gsettings set org.gnome.desktop.background picture-uri /home/fundo_ifsc_sj.jpg
-gsettings set org.cinnamon.desktop.background picture-uri  "file:///home/fundo_ifsc_sj.jpg"
